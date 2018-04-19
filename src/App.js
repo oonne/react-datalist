7eafe80cb9c58795e15aec613454626fc9b05951
JAY@oonne.com <JAY@oonne.com>
2018-04-19 19:56:24 +0800

    React DataList

--- src/App.js
+++ src/App.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import Datalist from './component/Datalist'
 
+let list = [
+  '000001',
+  '000002',
+  '600000',
+  '600001',
+  '600002',
+  '600100',
+]
+
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+
+    this.onChangeCode = this.onChangeCode.bind(this)
+
+    this.state = {
+      code: '',
+    }
+  }
+
+  onChangeCode(code, event){
+    let val = code || event.target.value;
+    this.setState({
+      code: val
+    })
+  }
+
   render() {
+    let code = this.state.code;
+
     return (
       <div className="App">
-        <Datalist/>
+
+        <div className="demo">
+          <div className="demo_content">
+            <input type="text" placeholder="input code" value={code} onChange={this.onChangeCode.bind(this, null)} />
+            <Datalist
+              val={code}
+              list={list}
+              setVal={this.onChangeCode}
+            />
+          </div>
+        </div>
+        <div className="tips">
+          Input 000001
+        </div>
       </div>
     );
   }
--- src/component/Datalist.js
+++ src/component/Datalist.js
@@ -1,11 +1,90 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classname from 'classname';
 import './Datalist.css';
 
 class Datalist extends Component {
+  static propTypes = {
+      className: PropTypes.string,
+      val: PropTypes.string,
+      list: PropTypes.array,
+      setVal: PropTypes.func,
+      maxRow: PropTypes.number,
+  };
+
+  static defaultProps = {
+    val: '',
+    list: [],
+    setVal: ()=>{},
+    maxRow: 5,
+  };
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      show: false,
+    }
+  }
+  
+  componentWillReceiveProps(nextProps){
+    let val = nextProps.val;
+    let list = this.getList(nextProps.list, val);
+
+    let hit = list.some(item=>item==val);
+
+    if(val=='' || list.length==0 || hit){
+      this.hide();
+    }else{
+      this.show();
+    }
+  }
+
+  show () {
+    this.setState({
+      show: true
+    })
+  }
+
+  hide () {
+    this.setState({
+      show: false
+    })
+  }
+
+  getList(list, val, maxRow=this.props.maxRow){
+    let associative = list.filter(item=>{
+      if (item){
+        return item.indexOf(val)>=0;
+      }else{
+        return false
+      }
+    })
+
+    if (associative.length>maxRow) {
+      associative = associative.slice(0, maxRow)
+    }
+
+    return associative;
+  }
+
+  renderLi(item){
+    return(
+      <li key={item} onClick={this.props.setVal.bind(null, item)}>
+        {item}
+      </li>
+    )
+  }
+
   render() {
+    let datalistClass = classname("datalist", this.props.className, {'hidden': !this.state.show});
+    let list = this.getList(this.props.list, this.props.val);
+
     return (
-      <div className="datalist">
-        TODO
+      <div className={datalistClass}>
+        <ul>
+          {list.map(item=>this.renderLi(item))}
+        </ul>
       </div>
     );
   }
